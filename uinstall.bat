833255694ab92682b8abf7704046aaf59e317078
91en <wo20ljj@gmail.com>
2020-06-07 11:24:50 +0800

    update

--- uinstall.bat
+++ uinstall.bat
@@ -4,8 +4,10 @@ cls
 color 0a
 
 echo. 正在卸载程序……请耐心等待
-rd /s /q "c:/Red Rocket Readers"
 del /q /f "c:\Users\%username%\Desktop\打开红火箭.lnk"
+rd /s /q "c:/Red Rocket Readers"
 cls
-echo. 卸载完成，按任意键退出……或直接点击右上角“X”按钮
+
+echo. 卸载完成，请手动删除不需要的文件夹
+echo. 按任意键退出……或直接点击右上角“X”按钮
 pause >nul
